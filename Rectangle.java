a7d5922f9305ba9587d403a3890226e1e1ebc0c0
Ekechi123 <your_email@example.com>
2024-11-27 15:51:13 -0500

    Added Rectangle.java with basic structure

---
+++ Rectangle.java
@@ -0,0 +1,32 @@
+public class Rectangle {
+    private double x, y, width, height;
+
+    public Rectangle(double x, double y, double width, double height) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    @Override
+    public String toString() {
+        return "Rectangle at (" + x + ", " + y + "): " + width + "x" + height;
+    }
+}
+
